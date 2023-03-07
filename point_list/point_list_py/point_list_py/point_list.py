bccd68f83b1db96eab968a51c76c5d286a91e41e
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2023-03-07 10:27:47 +0100

    Added get_vehicle_position function

---
+++ point_list/point_list_py/point_list_py/point_list.py
@@ -0,0 +1,178 @@
+"""
+Python implementation of Offboard Control
+
+"""
+
+import rclpy
+import numpy as np
+from rclpy.node import Node
+from rclpy.clock import Clock
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
+
+from px4_msgs.msg import OffboardControlMode
+from px4_msgs.msg import TrajectorySetpoint
+from px4_msgs.msg import VehicleCommand
+from px4_msgs.msg import VehicleLocalPosition
+
+
+class OffboardControl(Node):
+
+    def __init__(self):
+        super().__init__('OffboardControl')
+
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+            durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+            history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+            depth=1
+        )
+
+        self.vehicle_local_position_subscriber_ = self.create_subscription(VehicleLocalPosition, 
+                                                                       "/fmu/out/vehicle_local_position", self.get_vehicle_position, qos_profile)
+        self.offboard_control_mode_publisher_ = self.create_publisher(OffboardControlMode,
+                                                                        "/fmu/in/offboard_control_mode", qos_profile)
+        self.trajectory_setpoint_publisher_ = self.create_publisher(TrajectorySetpoint,
+                                                                    "/fmu/in/trajectory_setpoint", qos_profile)
+        self.vehicle_command_publisher_ = self.create_publisher(VehicleCommand, 
+                                                                "/fmu/in/vehicle_command", qos_profile)
+        
+        self.offboard_setpoint_counter_ = 0
+
+        timer_period = 0.1  # 100 milliseconds
+        self.timer_ = self.create_timer(timer_period, self.timer_callback)
+
+        self.radius = 5
+        self.omega = 0.5
+        self.theta = 0.0
+        self.dt = timer_period
+        self.land_to_initial_position = True
+
+
+    def timer_callback(self):
+        if (self.offboard_setpoint_counter_ == 10):
+            # Change to Offboard mode after 10 setpoints
+            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 1., 6.)
+            # Arm the vehicle
+            self.arm()
+
+        # Offboard_control_mode needs to be paired with trajectory_setpoint
+        self.publish_offboard_control_mode()
+        self.publish_trajectory_setpoint()
+
+        if(self.offboard_setpoint_counter_ == 300):
+            # land and disarm after 30 seconds
+            if(self.land_to_initial_position == False):
+                self.land()
+            else:
+                self.land_to_initial_pos()
+                self.get_logger().info("Landing to initial position")
+            
+            self.disarm()
+
+        self.offboard_setpoint_counter_ += 1
+
+    # Arm the vehicle
+    def arm(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0)
+        self.get_logger().info("Arm command send")
+
+    # Disarm the vehicle
+    def disarm(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0)
+        self.get_logger().info("Disarm command send")
+
+    # Land
+    def land(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
+        self.get_logger().info("Land command sent")
+
+    # Land to initial position
+    def land_to_initial_pos(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND, 0.0, 0.0, 0.0, np.pi, 0.0, 0.0, 0.0)
+        self.get_logger().info("Landing to initial position")   
+
+    '''
+	Publish the offboard control mode.
+	For this example, only position and altitude controls are active.
+    '''
+
+    def publish_offboard_control_mode(self):
+        msg = OffboardControlMode()
+        msg.position = True
+        msg.velocity = False
+        msg.acceleration = False
+        msg.attitude = False
+        msg.body_rate = False
+        msg.timestamp = int(Clock().now().nanoseconds / 1000) # time in microseconds
+        self.offboard_control_mode_publisher_.publish(msg)
+        
+
+    '''
+	Publish a trajectory setpoint
+	For this example, it sends a trajectory setpoint to make the
+	vehicle hover at 5 meters with a yaw angle of 180 degrees.
+    '''
+
+    def publish_trajectory_setpoint(self):
+        msg = TrajectorySetpoint()
+        #msg.timestamp = self.timestamp_
+
+        x = self.radius * np.cos(self.theta)
+        y = self.radius * np.sin(self.theta)
+        z = -5.0
+        msg.position = [x, y, z] 
+        msg.yaw = -3.14  # [-PI:PI]
+
+        msg.timestamp = int(Clock().now().nanoseconds / 1000) # time in microseconds
+        self.trajectory_setpoint_publisher_.publish(msg)
+        #self.get_vehicle_position()
+
+        self.theta = self.theta + self.omega * self.dt
+
+    '''
+    Publish vehicle commands
+        command   Command code (matches VehicleCommand and MAVLink MAV_CMD codes)
+        param1    Command parameter 1 as defined by MAVLink uint16 VEHICLE_CMD enum
+        param2    Command parameter 2 as defined by MAVLink uint16 VEHICLE_CMD enum
+    '''
+    def publish_vehicle_command(self, command, param1=0.0, param2=0.0, param3=0.0, param4=0.0, param5=0.0, param6=0.0, param7=0.0):
+        msg = VehicleCommand()
+        msg.param1 = param1
+        msg.param2 = param2
+        msg.param3 = param3
+        msg.param4 = param4
+        msg.param5 = param5
+        msg.param6 = param6
+        msg.param7 = param7
+        msg.command = command  # command ID
+        msg.target_system = 1  # system which should execute the command
+        msg.target_component = 1  # component which should execute the command, 0 for all components
+        msg.source_system = 1  # system sending the command
+        msg.source_component = 1  # component sending the command
+        msg.from_external = True
+        msg.timestamp = int(Clock().now().nanoseconds / 1000) # time in microseconds
+        self.vehicle_command_publisher_.publish(msg)
+
+    def get_vehicle_position(self, msg):
+        pos_x = msg.x
+        pos_y = msg.y
+        pos_z = msg.z
+
+        self.get_logger().info("Posizione attuale: ({:.2f}, {:.2f}, {:.2f})".format(pos_x, pos_y, pos_z))
+
+        
+def main(args=None):
+    rclpy.init(args=args)
+    print("Starting offboard control node...\n")
+    offboard_control = OffboardControl()
+    rclpy.spin(offboard_control)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    offboard_control.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- point_list/point_list_py/setup.py
+++ point_list/point_list_py/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'point_list_py = point_list_py.point_list:main',
         ],
     },
 )
