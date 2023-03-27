dbea95d0e8437b3ed4ae8a2f2c93428d42e04b9d
GiacomoCorradini <corra981@gmail.com>
2023-03-27 15:44:48 +0200

    Fix MicroXRCE

--- MicroXRCE.sh
+++ MicroXRCE.sh
@@ -3,4 +3,4 @@
 source ~/microros_ws/install/local_setup.bash
 source install/local_setup.bash
 
-ros2 run micro_ros_agent micro_ros_agent serial --dev /ttyS0 -b 921600 ROS_DOMAIN_ID=0
+ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyS0 -b 921600 ROS_DOMAIN_ID=0
