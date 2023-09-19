2d7b4d743e73d8df9ec77d5083ffb9fb2757f801
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2023-09-19 11:38:52 +0200

    Update executable files

--- gps/gps_cpp/run_point.sh
+++ gps/gps_cpp/run_point.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-source ~/px4_ros_com_ws/install/local_setup.bash
 source install/local_setup.bash
-
 ros2 run gps_cpp offboard_gps_position
--- gps/gps_py/run_point.sh
+++ gps/gps_py/run_point.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-source ~/px4_ros_com_ws/install/local_setup.bash
 source install/local_setup.bash
-
 ros2 run gps_py gps_position_py
