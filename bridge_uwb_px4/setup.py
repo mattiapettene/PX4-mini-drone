424e8e7c6da80c1244651f9da422ad3ed019f197
GiacomoCorradini <corra981@gmail.com>
2023-12-20 12:07:29 +0100

    Add bridge_uwb forlder

---
+++ bridge_uwb_px4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'bridge_uwb_px4'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='giacomo',
+    maintainer_email='corra981@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'uwb_bridge = bridge_uwb_px4.uwb_bridge:main'
+        ],
+    },
+)
