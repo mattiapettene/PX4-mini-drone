a3686611f1e9ebf6457242931412b152a456f488
GiacomoCorradini <corra981@gmail.com>
2023-12-30 14:14:16 +0100

    Update launch files to run mocap/uwb bridges

--- launch/mocap_offboard.launch.py
+++ launch/mocap_offboard.launch.py
@@ -3,13 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package="gps_py",
-        #     executable="gps_velocity_py",
-        #     name="gps_velocity_py",
-        #     output="screen",
-        #     emulate_tty=True,
-        # ),
         Node(
             package="qualisys_ros",
             executable="qualisys_node",
--- launch/uwb_offboard.launch.py
+++ launch/uwb_offboard.launch.py
@@ -3,29 +3,18 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package="gps_py",
-        #     executable="gps_velocity_py",
-        #     name="gps_velocity_py",
-        #     output="screen",
-        #     emulate_tty=True,
-        # ),
-        # Node(
-        #     package="qualisys_ros",
-        #     executable="qualisys_node",
-        #     name="qualisys_node",
-        #     output="screen",
-        #     emulate_tty=True,
-        #     arguments=[
-        #         "--server", "192.168.1.216",
-        #         "--rate", "100",
-        #     ],
-        # ),
-        # Node(
-        #     package="bridge_mocap_px4",
-        #     executable="mocap_bridge",
-        #     name="bridge_mocap_px4",
-        #     output="screen",
-        #     emulate_tty=True,
-        # )
+        Node(
+            package="bridge_uwb_px4",
+            executable="uwb_bridge",
+            name="uwb_bridge",
+            output="screen",
+            emulate_tty=True,
+        ),
+        Node(
+            package="bridge_uwb_px4",
+            executable="mb1202_bridge",
+            name="mb1202_bridge",
+            output="screen",
+            emulate_tty=True,
+        )
     ])
