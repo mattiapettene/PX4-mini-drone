a40bb3bdcae1dd18e263afd9e313468b66d580af
GiacomoCorradini <corra981@gmail.com>
2024-01-17 17:23:14 +0100

    Remove useless file

--- uwb_test.sh
+++
@@ -1,7 +0,0 @@
-#MICROXRCE
-directory=Micro-XRCE-DDS-Agent
-gnome-terminal --tab --working-directory="$directory" -- bash -c "MicroXRCEAgent udp4 -p 8888"
-
-#PX4
-directory=PX4-Autopilot
-gnome-terminal --tab --working-directory="$directory" -- bash -c "make px4_sitl gazebo-classic_iris PX4_SITL_WORLD=my_world_2"
